80189af85e182f37b58f92d41181f73145f0fcd3
Mohamed Faki <hammadi.faki@gmail.com>
2023-05-02 06:46:46 -0400

    Initialized functions for to get the time and date added usage, license, Roadmap and Author to the README

---
+++ analog-clock/index.js
@@ -0,0 +1,16 @@
+
+// function that declares variable myTime - to get the current time
+function myTime() {
+const milliSeconds = 0;
+const seconds = 0;
+const minutes = 0;
+const hours = 0;
+}
+
+// function that declares variable myDate - to get the current date
+function myDate() { 
+    const dayOfWeek = "blank"
+    const day = 0;
+    const month = 0;
+    const year = 0;
+}
